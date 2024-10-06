6c464636e209925f0e3a8554cfb915e2e62d5026
projmix <balb310372@mail.ru>
2024-10-06 23:41:33 +0300

    create code command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let result = '';
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace('--', '');
+        const value = args[i + 1];
+        result += `${propName} is ${value}`;
+        if (i + 2 < args.length) result += ', ';
+    }
+
+    console.log(result);
 };
 
 parseArgs();
