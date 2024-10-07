dedc5fb18e8fe4efcd68582b8a9acdc35f2f0f81
projmix <balb310372@mail.ru>
2024-10-07 16:16:08 +0300

    create code Streams (src/streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const stream = createReadStream(filePath, 'utf8');
+
+    stream.pipe(process.stdout);
+
+    stream.on('error', (err) => {
+        console.error('FS operation failed:', err.message);
+    });
+
+    stream.on('end', () => {
+        console.log('\nFile read complete.');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'fs';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
+
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+    const writableStream = createWriteStream(filePath);
+    
+    process.stdin.pipe(writableStream);
+    
+    writableStream.on('error', (err) => {
+        console.error('FS operation failed', err);
+    });
+
 };
 
 await write();
