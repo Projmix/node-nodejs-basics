84be419ae029da12dd55ffb3705536f1721eda85
projmix <balb310372@mail.ru>
2024-10-07 15:52:00 +0300

    create code Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    
+    const input = createReadStream(filePath);
+    input.on('data', (chunk) => hash.update(chunk));
+    
+    input.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+    
+    input.on('error', (err) => {
+        console.error('FS operation failed', err);
+    });
 };
 
 await calculateHash();
